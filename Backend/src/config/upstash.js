25dec33f47ccd8dc58eb500870d75d3960c3e7cf
Astral-Error <182073205+Astral-Error@users.noreply.github.com>
2026-03-04 16:07:56 +0530

    chore(note card ui)

--- Backend/src/config/upstash.js
+++ Backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(5,"60 s")
+    limiter: Ratelimit.slidingWindow(5,"5 s")
 });
 ;
 export default ratelimit;
